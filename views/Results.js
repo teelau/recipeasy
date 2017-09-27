00b04852415797b34240fd3226ac3abd5fef2656
Brian Nguyen <briannguyen992@gmail.com>
2017-09-26 20:26:34 -0700

    Displaying list of names

--- views/Results.js
+++ views/Results.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 
 export default class Results extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Results</Text>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Card name={item.key} />}
+        />
       </View>
     )
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Name: {this.props.name}</Text>
+      </View>
+    );
   }
 }
 
@@ -17,5 +39,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+  item: {
+
   },
 });
