ce731a2ba7f704edb1926b210b53a82db071fb29
Brian Nguyen <briannguyen992@gmail.com>
2017-10-26 23:18:42 -0700

    Space

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<Provider store = {this.store}>
+			<Provider store={this.store}>
 				<AppWithNavigationState />
 			</Provider>
 		);
