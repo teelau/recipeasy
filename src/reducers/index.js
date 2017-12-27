59f8d1cbc0d7331bf7de4a6f376cd61ee467532b
Brian Nguyen <briannguyen992@gmail.com>
2017-12-26 16:28:53 -0800

    Remove extraneous files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import { NavReducer } from './NavReducer';
 import { IngredientReducer } from './IngredientReducer';
 import { RecipeReducer } from './RecipeReducer';
 
 const rootReducer = combineReducers({
-  NavReducer,
   IngredientReducer,
   RecipeReducer
 });
